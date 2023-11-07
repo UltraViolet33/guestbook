11be2c3c9678c755fa365eaf7d22884d5ce28638
UltraViolet33 <93035200+UltraViolet33@users.noreply.github.com>
2023-11-07 12:09:31 +0100

    display show page for conference

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use App\Repository\CommentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\ConferenceRepository;
+use App\Entity\Conference;
 
 class ConferenceController extends AbstractController
 {
@@ -13,5 +15,12 @@ class ConferenceController extends AbstractController
     public function index(ConferenceRepository $conferenceRepository): Response
     {
         return $this->render('conference/index.html.twig', ["conferences" => $conferenceRepository->findAll()]);
+    }
+
+
+    #[Route('/conference/{id}', name: 'conference')]
+    public function show(Conference $conference, CommentRepository $commentRepository): Response 
+    {
+        return $this->render('conference/show.html.twig', ["conference" => $conference, "comments" => $commentRepository->findBy(["conference" => $conference])]);
     }
 }
