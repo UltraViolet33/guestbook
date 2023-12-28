42cdcb9af14e58d764c3031f0c606f090c91ae0b
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-12-28 08:24:35 +0100

    display conferences and comments in spa

--- spa/src/app.js
+++ spa/src/app.js
@@ -1,9 +1,23 @@
 import { h, render } from "preact";
 import { Router, Link } from "preact-router";
+import { useState, useEffect } from "preact/hooks";
+
 import Home from "./pages/home";
 import Conference from "./pages/conference";
 import "../assets/styles/app.scss";
+import { findConferences } from "./api/api";
+
 function App() {
+  const [conferences, setConferences] = useState(null);
+
+  useEffect(() => {
+    findConferences().then((conferences) => setConferences(conferences));
+  }, []);
+
+  if (conferences === null) {
+    return <div className="text-center pt-5">Loading...</div>;
+  }
+
   return (
     <div>
       <header className="header">
@@ -15,15 +29,19 @@ function App() {
           </div>
         </nav>
         <nav className="bg-light border-bottom text-center">
-          <Link className="nav-conference" href="/conference/amsterdam2019">
-            Amsterdam 2019
-          </Link>
+          {conferences.map((conference) => (
+            <Link
+              className="nav-conference"
+              href={"/conference/" + conference.slug}>
+              {conference.city} {conference.year}
+            </Link>
+          ))}
         </nav>
       </header>
 
       <Router>
-        <Home path="/" />
-        <Conference path="/conference/:slug" />
+        <Home path="/" conferences={conferences} />
+        <Conference path="/conference/:slug" conferences={conferences} />
       </Router>
     </div>
   );
